869a569e1d8e69034d2f6eafe03eaea1e01d0dd6
Yooaknow <dbdkfma1228@naver.com>
2025-10-20 15:57:59 +0900

    feat: 오류 메시지 상수 파일(errors.js) 추가
    - 모든 에러 메시지를 중앙에서 관리하기 위한 errors.js 파일 생성

---
+++ src/calculator/errors.js
@@ -0,0 +1,9 @@
+export const ERR = {
+  BAD_CUSTOM: "커스텀 구분자 형식이 올바르지 않습니다.",
+  BAD_DELIM: "잘못된 구분자 형식입니다.",
+  BAD_SPACE: "숫자 사이에 공백이 있습니다.",
+  CANNOT_PARSE: "변환할 수 없는 값이 있습니다.",
+  NON_NUM: "숫자만 입력 가능합니다.",
+  NEGATIVE: "음수는 허용되지 않습니다.",
+  UNSAFE: "안전한 정수 범위를 초과했습니다.",
+};
